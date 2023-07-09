b4f4da5486b431cde7c2551d6aeae1ac36329e25
Алексей Бельченков <u608110@gmail.com>
2023-07-09 14:04:25 +0300

    Make router class

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
 
