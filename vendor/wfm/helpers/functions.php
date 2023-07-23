72218d76d5b091d92ad60f85468fe5e48f682616
Алексей Бельченков <u608110@gmail.com>
2023-07-23 12:56:44 +0300

    Check language in url

--- vendor/wfm/helpers/functions.php
+++ vendor/wfm/helpers/functions.php
@@ -12,3 +12,16 @@ function h($str)
 {
     return htmlspecialchars($str);
 }
+
+function redirect($http = false)
+{
+    if ($http) {
+        $redirect = $http;
+    } else {
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    }
+
+    header("Location: $redirect");
+    
+    die;
+}
