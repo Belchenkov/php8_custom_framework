0311ac2834fe42e5cbbfd8c6904d66e8cbc4fa46
Алексей Бельченков <u608110@gmail.com>
2023-09-09 15:43:53 +0300

    cart checkout

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 
 use app\models\Cart;
 use wfm\App;
+use app\models\User;
 
 /** @property Cart $model */
 class CartController extends AppController
@@ -71,6 +72,36 @@ class CartController extends AppController
         $this->loadView('cart_modal');
         
         return true;
+    }
+
+    public function viewAction()
+    {
+        $this->setMeta(___('tpl_cart_title'));
+    }
+
+    public function checkoutAction()
+    {
+        if (!empty($_POST)) {
+            // регистрация пользователя, если не авторизован
+            if (!User::checkAuth()) {
+                $user = new User();
+                $data = $_POST;
+                $user->load($data);
+                if (!$user->validate($data) || !$user->checkUnique()) {
+                    $user->getErrors();
+                    $_SESSION['form_data'] = $data;
+                    redirect();
+                } else {
+                    $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+                    if (!$user_id = $user->save('user')) {
+                        $_SESSION['errors'] = ___('cart_checkout_error_register');
+                        redirect();
+                    }
+                }
+            }
+
+        }
+        redirect();
     }
 
 }
