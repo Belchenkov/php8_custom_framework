6e0eac3b6a2aaac6d17b3e0fb3ce7462b7d183ab
Алексей Бельченков <u608110@gmail.com>
2023-09-21 14:42:46 +0300

    Protected admin

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -8,6 +8,7 @@ use app\models\AppModel;
 use app\widgets\language\Language;
 use wfm\App;
 use wfm\Controller;
+use app\models\admin\User;
 
 class AppController extends Controller
 {
@@ -17,6 +18,11 @@ class AppController extends Controller
     public function __construct($route)
     {
         parent::__construct($route);
+
+        if (!User::isAdmin() && $route['action'] != 'login-admin') {
+            redirect(ADMIN . '/user/login-admin');
+        }
+
         new AppModel();
         App::$app->setProperty('languages', Language::getLanguages());
         App::$app->setProperty('language', Language::getLanguage(App::$app->getProperty('languages')));
