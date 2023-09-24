bfa99cb3a09eb7e590890038818d28cff2a605d9
Алексей Бельченков <u608110@gmail.com>
2023-09-24 14:33:02 +0300

    Detele category

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -3,6 +3,7 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
 
 class CategoryController extends AppController
 {
@@ -14,4 +15,29 @@ class CategoryController extends AppController
         $this->set(compact('title'));
     }
 
+    public function deleteAction()
+    {
+        $id = get('id');
+        $errors = '';
+        $children = R::count('category', 'parent_id = ?', [$id]);
+        $products = R::count('product', 'category_id = ?', [$id]);
+
+        if ($children) {
+            $errors .= 'Ошибка! В категории есть вложенные категории<br>';
+        }
+
+        if ($products) {
+            $errors .= 'Ошибка! В категории есть товары<br>';
+        }
+
+        if ($errors) {
+            $_SESSION['errors'] = $errors;
+        } else {
+            R::exec("DELETE FROM category WHERE id = ?", [$id]);
+            R::exec("DELETE FROM category_description WHERE category_id = ?", [$id]);
+            $_SESSION['success'] = 'Категория удалена';
+        }
+
+        redirect();
+    }
 }
