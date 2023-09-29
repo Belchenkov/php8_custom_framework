e3091fb457f9d5f76f711314e8cdfab257edcbc2
Алексей Бельченков <u608110@gmail.com>
2023-09-29 10:00:51 +0300

    Edit category - update action

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -67,17 +67,24 @@ class CategoryController extends AppController
         $id = get('id');
 
         if (!empty($_POST)) {
-
-        }
-
-        $category = $this->model->get_category($id);
+            if ($this->model->category_validate()) {
+                if ($this->model->update_category($id)) {
+                    $_SESSION['success'] = 'Категория обновлена';
+                } else {
+                    $_SESSION['errors'] = 'Ошибка!';
+                }
+            }
         
+            redirect();
+        }
+        
+        $category = $this->model->get_category($id);
         if (!$category) {
             throw new \Exception('Not found category', 404);
         }
-
-        $lang = App::$app->getProperty('language')['id'];
         
+        $lang = App::$app->getProperty('language')['id'];
+
         App::$app->setProperty('parent_id', $category[$lang]['parent_id']);
         
         $title = 'Редактирование категории';
@@ -85,4 +92,5 @@ class CategoryController extends AppController
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title', 'category'));
     }
+
 }
