0c7f3cdc28a5eca551413ab13f871605ad2105e1
Алексей Бельченков <u608110@gmail.com>
2023-10-10 11:06:03 +0300

    working with order

--- app/controllers/admin/OrderController.php
+++ app/controllers/admin/OrderController.php
@@ -24,9 +24,32 @@ class OrderController extends AppController
 
         $orders = $this->model->get_orders($start, $perpage, $status);
         $title = 'Список заказов';
-        
+
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title', 'orders', 'pagination', 'total'));
     }
 
+    public function editAction()
+    {
+        $id = get('id');
+
+        if (isset($_GET['status'])) {
+            $status = get('status');
+            if ($this->model->change_status($id, $status)) {
+                $_SESSION['success'] = 'Статус заказа изменен';
+            } else {
+                $_SESSION['errors'] = 'Ошибка изменения статуса заказа';
+            }
+        }
+
+        $order = $this->model->get_order($id);
+
+        if (!$order) {
+            throw new \Exception('Not found order', 404);
+        }
+
+        $title = "Заказ № {$id}";
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'order'));
+    }
 }
