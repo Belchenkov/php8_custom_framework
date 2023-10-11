06c75ce12d9074ba133f4f5e0a23b293f533e6fb
Алексей Бельченков <u608110@gmail.com>
2023-10-11 15:07:11 +0300

    Output list of users

--- app/models/admin/User.php
+++ app/models/admin/User.php
@@ -2,6 +2,7 @@
 
 namespace app\models\admin;
 
+use RedBeanPHP\R;
 class User extends \app\models\User
 {
 
@@ -10,4 +11,8 @@ class User extends \app\models\User
         return (isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin');
     }
 
+    public function get_users($start, $perpage): array
+    {
+        return R::findAll("user", "LIMIT $start, $perpage");
+    }
 }
