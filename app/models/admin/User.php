cef36234df9b97634418593705a264db5cd78448
Алексей Бельченков <u608110@gmail.com>
2023-10-13 16:13:10 +0300

    Profile edit info

--- app/models/admin/User.php
+++ app/models/admin/User.php
@@ -3,8 +3,34 @@
 namespace app\models\admin;
 
 use RedBeanPHP\R;
+
 class User extends \app\models\User
 {
+
+    public array $attributes = [
+        'email' => '',
+        'password' => '',
+        'name' => '',
+        'address' => '',
+        'role' => '',
+    ];
+
+    public array $rules = [
+        'required' => ['email', 'password', 'name', 'address', 'role',],
+        'email' => ['email',],
+        'lengthMin' => [
+            ['password', 6],
+        ],
+        'optional' => ['password'],
+    ];
+
+    public array $labels = [
+        'email' => 'E-mail',
+        'password' => 'Пароль',
+        'name' => 'Имя',
+        'address' => 'Адрес',
+        'role' => 'Роль',
+    ];
 
     public static function isAdmin(): bool
     {
@@ -19,5 +45,18 @@ class User extends \app\models\User
     public function get_user($id): array
     {
         return R::getRow("SELECT * FROM user WHERE id = ?", [$id]);
+    }
+
+    public function checkEmail($user_data): bool
+    {
+        if ($user_data['email'] == $this->attributes['email']) {
+            return true;
+        }
+        $user = R::findOne('user', 'email = ?', [$this->attributes['email']]);
+        if ($user) {
+            $this->errors['unique'][] = 'Этот email уже используется другим пользователем';
+            return false;
+        }
+        return true;
     }
 }
